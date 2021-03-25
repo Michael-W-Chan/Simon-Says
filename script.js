8f5e8180fd9bde14849762d307e4ca0d1c6f49a1
Glitch (hollow-thundering-moustache) <none>
2021-03-25 03:37:36 +0000

    🌨⛳️ Updated with Glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+//Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0; 
+var gamePlaying = false;
+function startGame(){
+    //initialize game variables
+    progress = 0;
+    gamePlaying = true;
+}
